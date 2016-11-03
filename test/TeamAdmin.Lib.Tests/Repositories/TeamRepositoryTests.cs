2163a4395ee2a1023eab450cdf3fdb659e95111d
r15h1 <tapsee.rishi@gmail.com>
2016-11-03 17:06:53 -0400

    #1 - assert that new team gets new id when saved to db

--- test/TeamAdmin.Lib.Tests/Repositories/TeamRepositoryTests.cs
+++ test/TeamAdmin.Lib.Tests/Repositories/TeamRepositoryTests.cs
@@ -21,9 +21,17 @@ namespace TeamAdmin.Lib.Tests.Repositories
             public void ValuesArePersistedOnCreate()
             {
                 Team team = CreateTeamWithNoId();
-                Team savedTeam = repo.Save(team);
-                Assert.Equal(team.Name, savedTeam.Name);
-                Assert.Equal(team.ClubId, savedTeam.ClubId);
+                Team newTeam = repo.Save(team);
+                Assert.Equal(team.Name, newTeam.Name);
+                Assert.Equal(team.ClubId, newTeam.ClubId);
+            }
+
+            [Fact]
+            public void NewIdIsObtainedOnCreate()
+            {
+                Team team = CreateTeamWithNoId();
+                Team newTeam = repo.Save(team);
+                Assert.True(newTeam.TeamId.HasValue && newTeam.TeamId.Value > 0);
             }
 
             private Team CreateTeamWithNoId()
