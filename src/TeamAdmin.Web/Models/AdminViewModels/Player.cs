3589bb80a3e8e6fca6643cd2a446adef82239215
r15h1 <tapsee.rishi@gmail.com>
2017-05-15 11:14:59 -0400

    #56 - add player sql length issue

--- src/TeamAdmin.Web/Models/AdminViewModels/Player.cs
+++ src/TeamAdmin.Web/Models/AdminViewModels/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using TeamAdmin.Core;
 
 namespace TeamAdmin.Web.Models.AdminViewModels
@@ -8,15 +9,31 @@ namespace TeamAdmin.Web.Models.AdminViewModels
     {
         public int TeamId { get; set; }
         public int? PlayerId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string PhotoUrl { get; set; }
         public int? SquadNumber { get; set; }
+
+        [MaxLength(50)]
         public string Position { get; set; }
+
+        [MaxLength(500)]
         public string Address { get; set; }
+
+        [MaxLength(50)]
         public string City { get; set; }
+
+        [MaxLength(50)]
         public string Province { get; set; }
+
+        [MaxLength(6)]
         public string PostalCode { get; set; }
         public List<ContactInfo> ContactInfo { get; set; }    
     }
